dbcac164be3bd7cbeaf1af5c039cc05446b68c48
Arun Giridharan <giridharan.arun123@gmail.com>
2017-11-08 21:52:11 +0530

    added module export line
    Signed-off-by: Karthi Jagatheesan <karthijey@yahoo.com>

--- server/controllers/newPasswordController.js
+++ server/controllers/newPasswordController.js
@@ -1,1 +1,10 @@
 const UserModel = require('../database/models/User')
+
+const newPasswordController = {
+  _init (User = UserModel) {
+    this.User = User
+    return this
+  }
+}
+
+module.exports = newPasswordController
